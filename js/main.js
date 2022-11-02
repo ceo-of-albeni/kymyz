5df9963955d5446c21be9a6ff76645f1285428c6
Aliia <aliia@MacBook-Air-Aliia.local>
2022-11-02 22:51:34 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function render() {
+  let workers_div = document.querySelector("#workers");
+  workers_div.innerHTML = "";
+  fetch(`https://api.waifu.im/random/`)
+    .then(res => res.json())
+    .then(data =>
+      data.images.forEach(item => {
+        workers_div.innerHTML += `
+        <div class="card m-5" style="width: 18rem;">
+          <img src="${item.url}" class="card-img-top" width="300" height="300">
+        </div>`;
+      })
+    );
+}
+render();
+
+let btn_fetch = document.querySelector("#others");
+btn_fetch.addEventListener("click", render);
+
+let clicker_btn = document.querySelector("#like");
+let clicker_count = document.querySelector("#like_count");
+let count = 0;
+
+clicker_btn.addEventListener("click", () => {
+  (count += 1), (clicker_count.innerText = `${count} likes`);
+});
